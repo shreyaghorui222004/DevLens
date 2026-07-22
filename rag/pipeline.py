75f17b174085876cd4263d9e48b5081a7971bbc6
Anik Chand <anikchand461@gmail.com>
2026-07-22 11:26:40 +0530

    solved context issue

--- rag/pipeline.py
+++ rag/pipeline.py
@@ -4,11 +4,14 @@ from rag.chunker import Chunker
 from rag.vector_store import VectorStore
 from rag.retriever import Retriever
 from rag.llm import LLM
+from pathlib import Path
 
 
 class RAGPipeline:
 
     def __init__(self, json_path):
+        self.json_path = json_path
+        self.repo_name = Path(json_path).stem
         self.loader = JSONLoader(json_path)
         self.converter = DocumentConverter()
         self.chunker = Chunker()
@@ -25,4 +28,16 @@ class RAGPipeline:
 
     def ask(self, question):
         docs = self.retriever.retrieve(question)
-        return self.llm.generate(question, docs)
+        # print("\n========== RETRIEVED DOCUMENTS ==========\n")
+        
+        # for i, doc in enumerate(docs, 1):
+        #     print(f"Document {i}")
+        #     print(doc.metadata)
+        #     print(doc.page_content[:500])
+        #     print("-" * 80)
+        
+        return self.llm.generate(
+            question,
+            docs,
+            self.repo_name
+        )
